51a083674eaa878125ba67ab183afc6b9ae3fafc
Helen <helen@Helens-MacBook-Pro.local>
2018-07-14 11:48:48 -0500

    added 2nd part of the exercise

--- overly-excited.js
+++ overly-excited.js
@@ -29,5 +29,36 @@ function addExcitement (theWordArray) {
 
 }
 
+
+//Adding an ! after every third word.  
 // Invoke the function and pass in the array
 addExcitement(sentence);
+
+
+
+/*
+    The addExcitement function should be an impure function, and accept
+    the array as the sole argument. It should iterate over the array
+    and output the words to the browser console.
+*/
+function addExcitement (theWordArray) {
+
+    // Each time the for loop executes, you're going to add one more word to this string
+    let buildMeUp = ""
+
+    for (let i = 0; i < theWordArray.length; i++) {
+        // Concatenate the new word onto buildMeUp
+        buildMeUp = buildMeUp += theWordArray[i];
+        // Print buildMeUp to the console
+        console.log(buildMeUp);
+
+        if((i+1) % 3 === 0) {
+            buildMeUp = buildMeUp + "!";
+
+        }
+    }
+
+}
+// Invoke the function and pass in the array
+addExcitement(sentence);
+
